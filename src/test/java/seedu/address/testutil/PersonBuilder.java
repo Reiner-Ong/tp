b0f1e0cad8031f33cd720ac746a28a9934765afd
nrysho <neryssho@gmail.com>
2026-03-18 17:37:20 +0800

    Update PersonBuilder.java to include tour in Person constructor

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,12 +3,14 @@ package seedu.address.testutil;
 import seedu.address.model.contact.Contact;
 import seedu.address.model.contact.Person;
 
+import java.util.Collections;
+
 /**
  * A utility class to help with building Person objects.
  */
 public class PersonBuilder extends ContactBuilder {
     @Override
     public Contact build() {
-        return new Person(name, phone, email, address, tags);
+        return new Person(name, phone, email, address, tags, Collections.emptySet());
     }
 }
