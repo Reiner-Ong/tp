ec32548850137f85f75ee91ceb985d7dfc683b76
Ash Chen <94738775+Ash-L7@users.noreply.github.com>
2026-04-08 03:31:56 +0800

    Remove line causing differences between actual and filtered tour index

--- src/main/java/seedu/address/logic/commands/tour/TourViewCommand.java
+++ src/main/java/seedu/address/logic/commands/tour/TourViewCommand.java
@@ -42,7 +42,6 @@ public class TourViewCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        model.updateFilteredTourList(PREDICATE_SHOW_ALL_TOURS);
         List<Tour> tourList = model.getFilteredTourList();
 
         if (targetIndex.getZeroBased() >= tourList.size()) {
