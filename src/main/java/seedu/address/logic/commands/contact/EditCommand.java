0a5f884113f9e33c6174d18e6bd654a4b4c6f20d
Reiner Ong <e1337575@u.nus.edu>
2026-04-14 00:01:18 +0800

    Remove unused import in EditCommand

--- src/main/java/seedu/address/logic/commands/contact/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/contact/EditCommand.java
@@ -11,7 +11,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_OPENING_HOUR;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STARS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
-import static seedu.address.model.Model.PREDICATE_SHOW_ALL_CONTACTS;
 
 import java.util.Collections;
 import java.util.HashSet;
